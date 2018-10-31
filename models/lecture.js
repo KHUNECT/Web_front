822c6b8038da405d7ab2c3119239247b9ca4ec6e
박민재 <sobu0715@gmail.com>
2018-10-31 19:33:58 +0900

    #2 Back-Commit

--- models/lecture.js
+++ models/lecture.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const LectureSchema = new Schema({
-    lectureId = {type: String, required: true, unique: true},
-    title = {type: String, required: true},
+    lectureId : {type: String, required: true, unique: true},
+    title : {type: String, required: true},
     professor = String
 },{collection: "lectures"}
 )
