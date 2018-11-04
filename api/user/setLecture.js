8b8b4dd229154eb9afbc77cf7a0293e7e58342e7
박민재 <sobu0715@gmail.com>
2018-11-05 03:29:12 +0900

    #6 Back Commit

--- api/user/setLecture.js
+++ api/user/setLecture.js
@@ -1,3 +1,15 @@
 'use strict'
 
 const User = require('../../models/user')
+const Post = require('../../models/post')
+const Board = require('../../models/board')
+
+exports.SetLecture = (req, res) => {
+    const userId = req.body.userId
+    const klasId = req.body.klasId
+    const klasPassword = req.body.klasPassword
+
+    let options = {
+
+    }
+}
