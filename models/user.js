bca132ec2dc959df88b75fea13ee7145e5482b93
박민재 <sobu0715@gmail.com>
2018-11-15 03:18:04 +0900

    #30 Back Commit (ZZinMak)

--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-const bcrypt=require('bcrypt-nodejs')
+const bcrypt = require('bcrypt-nodejs')
 const Schema = mongoose.Schema
 
 const UserSchema = new Schema({
     name :{type:String,required:true},
     userId : {type: String, required: true, unique: true, lowercase: true},
-    password : {type: String, required: true, trim: true},
+    password : {type: String, trim: true},
     nickname : {type: String, required: true, unique: true},
     email : {type: String, required: true, unique: true},
     major : {type: String, required: true},
@@ -21,15 +21,16 @@ UserSchema.methods.generateHash=function(password){
 }
 
 UserSchema.methods.validPassword=function(password){
-    if(this.password!=null)
-        return bcrypt.compareSync(password,this.password)
+    if(this.password != null)
+        return bcrypt.compareSync(password, this.password)
     else
         return false
 }
-
+/*
 UserSchema.pre('save',function(next){
     this.password=bcrypt.hashSync(this.password,bcrypt.genSaltSync(10),null)
     next()
 })
+*/
 
 module.exports = mongoose.model('User', UserSchema)
